02b7aafe5276721a710419f3cea4b7114dc488b7
Angelica Flores <martha.fuentes@broxel.com>
2020-06-12 03:19:10 -0500

    Cambios sitio Broxel

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="layout">
     <header class="header">
-      <strong>
-        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
-      </strong>
+      <g-image src="~/assets/logoBB.png" width="200" />
+
       <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about/">About</g-link>
+        <g-link class="nav__link" to="/">Inicio</g-link>
+        <g-link class="nav__link" to="/">Personas</g-link>
+        <g-link class="nav__link" to="/">Empresas</g-link>
       </nav>
+      <span class="button-login">Iniciar sesión</span>
     </header>
-    <slot/>
+    <slot />
   </div>
 </template>
 
@@ -23,14 +24,15 @@ query {
 
 <style>
 body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
+  font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Roboto,
+    "Helvetica Neue", Arial, sans-serif;
+  margin: 0;
+  padding: 0;
   line-height: 1.5;
 }
 
 .layout {
-  max-width: 760px;
+  max-width: 960px;
   margin: 0 auto;
   padding-left: 20px;
   padding-right: 20px;
@@ -44,7 +46,23 @@ body {
   height: 80px;
 }
 
+.button-login {
+  background-color: #3298dc;
+  color: white;
+  font-weight: bold;
+  padding: 0.5rem;
+  border-radius: 5px;
+}
+
 .nav__link {
   margin-left: 20px;
+  text-decoration: none;
+  color: black;
+}
+
+.left,
+.right {
+  width: 50%;
+  display: inline-block;
 }
 </style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,33 +1,94 @@
 <template>
   <Layout>
-
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
-
-    <h1>Hello, world!</h1>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
-
+    <section class="top">
+      <div class="left">
+        <h1>Llegó la era de la Transformación Broxel 5.0; ya nada será igual.</h1>
+        <p>
+          No te quedes fuera de la Transformación que marcará un antes y un después en medios de
+          pago. Regístrate con tu Email en 5 segundos en esta lista de espera y gana dinero al invitar
+          a más personas.
+        </p>
+        <form>
+          <div class="right">
+            <input type="text" placeholder="Ingresa tu Email" />
+          </div>
+          <div class="left">
+            <button class="cta" type="submit">Únete ahora</button>
+          </div>
+        </form>
+      </div>
+      <div class="right">
+        <g-image src="~/assets/wl2-1.png" width="400" />
+      </div>
+    </section>
+    <section class="middle">
+      <div class="left">
+        <h1>Sé de los primeros usuarios privilegiados en Transformar el dinero en algo “más valioso”.</h1>
+        <p>
+          Cada vez que invitas a alguien a nuestra
+          Transformación buenas cosas pasan, por ejemplo, tienes acceso a meses gratis en Netflix, Spotify o Amazon Prime.
+        </p>
+        <p>
+          <strong>Uno, tres y doce meses gratis de tu plataforma favorita.</strong>
+        </p>
+        <g-image src="~/assets/icon.png" width="200" />
+      </div>
+      <div class="left">
+        <g-image class="video" src="~/assets/video.png" width="400" />
+      </div>
+    </section>
+    <footer>
+      <p>
+        <small>
+          Broxel by Broxel. The source code is licensed.
+          <br />2020
+        </small>
+      </p>
+    </footer>
   </Layout>
 </template>
 
 <script>
 export default {
   metaInfo: {
-    title: 'Hello, world!'
+    title: "Hello, world!"
   }
-}
+};
 </script>
 
 <style>
 .home-links a {
   margin-right: 1rem;
+}
+
+p,
+h1,
+form {
+  padding: 20px;
+}
+
+footer {
+  text-align: center;
+}
+
+.video {
+  float: right;
+  margin: 20px;
+  margin-bottom: 40%;
+}
+.middle {
+  background-color: #f5f5f5;
+  width: 100% !important;
+  margin: 0 !important;
+  max-width: 100% !important;
+}
+.cta {
+  background-color: #3298dc;
+  color: white;
+  font-weight: bold;
+  padding: 0.5rem;
+  border-radius: 5px;
+  border: none;
 }
 </style>
